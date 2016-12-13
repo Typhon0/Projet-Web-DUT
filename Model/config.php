223c01be9970ae0faf507b43475145fd595c5dfd
Slinth <dorian.decroix@hotmail.fr>
2016-12-13 13:56:00 +0100

    Changement paramètres de connexion BDD

--- Model/config.php
+++ Model/config.php
@@ -1,5 +1,5 @@
 <?php
 
-	$bdd = new PDO('mysql:host=localhost;dbname=univ;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=86.105.212.158;dbname=ProjetWebDUT;charset=utf8', 'ProjetWebDUT', 'sVdWd4gt6VL01mOW');
 
 ?> 
