22c0f71eac3d7766a080099d231571818b411d41
Typhon0 <sculier.loic@gmail.com>
2016-12-13 14:44:39 +0100

    ajout menu

---
+++ View/menu.php
@@ -0,0 +1,28 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+            <a class="navbar-brand" href="#"> <img src="http://placehold.it/150x50&text=Logo" alt=""> </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li> <a href="#">Acceuil</a> </li>
+                <li> <a href="#">Annonce</a> </li>
+                <li> <a href="#">Mon agenda</a> </li>
+                <li> <a href="#">Forum</a> </li>
+                <li class="dropdown"> <a href="#" data-toggle="dropdown" class="dropdown-toggle">Informations <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Données médicales</a></li>
+                        <li><a href="#">Trouver où se soigner</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <li><a href="#" style="padding : 30px 0; color:#9d9d9d;" class="pull-right userlogin">Login</a> </li>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
