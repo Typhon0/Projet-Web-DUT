80bd7431469ac4bc6694c6422bad6582969925aa
Yuoox <royal.lastchaos@gmail.com>
2017-01-03 09:27:13 +0100

    Add files via upload

--- View/inscription.php
+++ View/inscription.php
@@ -5,17 +5,14 @@
 	</head>
 	<body>
 	 <?php echo '<p>veuillez vous inscrire</p>'; ?>
-		 <form action="../Controler/inscription_confirmation.php" method="post">
-		 <p><label>Pseudo : </label><input type="text" name="pseudo" required /></p>
-		 <p><label>Nom : </label><input type="text" name="nom" required /></p>
-		 <p><label>Prenom : </label><input type="text" name="prenom" required/></p>
-		 <p><label>Âge :</label><input type="text" name="age" required/></p>
-		 <p><label>Adresse : </label><input type="text" name="adresse" required/></p>
-		 <p><label>Code Postal : </label><input type="text" name="CP" required/></p>
-		 <p><label>Ville : </label><input type="text" name="ville" required/></p>
-		 <p><label>E-mail: </label><input type="text" name="email" required/></p>
-		 <p><label>Numéro de téléphone : </label><input type="text" name="telephone" required/></p>
-
+		<form action="../Controler/inscription_confirmation.php" method="post">
+		<p><label>Pseudo : </label><input type="text" name="pseudo" required /></p>
+		<p><label>E-mail : </label><input type="text" name="email" required/></p>
+		<p><label>Mot de passe: </label><input type="text" name="mdp" required /></p>
+		<p><label>description : </label><input type="text" name="desc" required/></p>
+		<p><label>date de naissance : </label><input type="date" name="ddn" required/></p>
+		<p><label>Type : </label><input type="text" name="type" required/></p>
+		
 	 <p><input type="submit" value="valider"></p>
 	</form>
 	
--- View/inscription_error.php
+++ View/inscription_error.php
@@ -4,7 +4,7 @@
 		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
 	<body>
-	 <?php echo 'Désolé, il y a eu une erreur lors de votre inscription. Le champ '.$sucessful.'est déjà utilisé'; ?>
+	 <?php echo 'Désolé, il y a eu une erreur lors de votre inscription. Le champ '.$sucessful.' est déjà utilisé'; ?>
 
 	</body>
 </html>
