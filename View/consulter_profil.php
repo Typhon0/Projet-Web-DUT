d0abafb42cd91bc5ec9eee75db953c8f6ff7ee14
Yuoox <royal.lastchaos@gmail.com>
2017-01-03 14:59:46 +0100

    Add files via upload

--- View/consulter_profil.php
+++ View/consulter_profil.php
@@ -4,19 +4,13 @@
 		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
 	<body>
-	 <?php echo '<p>Voici les informations à propos de '.$users1['pseudo'].'</p>'; ?>
+	 <?php echo '<p>Voici les informations à propos de '.$users1['login'].'</p>'; ?>
 		
-		 <p><label>Pseudo : </label> <?php echo $users1['pseudo'] ?></p>
-		 <p><label>Nom : </label><?php echo $users1['nom'] ?></p>
-		 <p><label>Prenom : </label><?php echo $users1['prenom'] ?></p>
-		 <p><label>Age :</label><?php echo $users1['age'] ?></p>
-		 <p><label>Adresse : </label><?php echo $users1['adresse'] ?></p>
-		 <p><label>Code Postal : </label><?php echo $users1['CP'] ?></p>
-		 <p><label>Ville : </label><?php echo $users1['ville'] ?></p>
-		 <p><label>E-mail: </label><?php echo $users1['email'] ?></p>
-		 <p><label>Numéro de téléphone : </label><?php echo $users1['telephone'] ?></p>
-
-	
-
+		 <p><label>Pseudo : </label> <?php echo $users1['login'] ?></p>
+		 <p><label>email : </label><?php echo $users1['email'] ?></p>
+		 <p><label>Description : </label><?php echo $users1['description'] ?></p>
+		 <p><label>Date de naissance : </label><?php echo $users1['dateNaiss'] ?></p>
+		 <p><label>type : </label><?php echo $users1['type'] ?></p>
+		 <p><label>Avatar :</label><?php echo $users1['avatar'] ?></p>
 	</body>
 </html>
