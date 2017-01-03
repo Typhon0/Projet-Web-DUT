c89ef995b98975901a52f7360b61aa54b43cdccd
AsTrag0ld <anthony.defossez03@gmail.com>
2017-01-03 10:09:05 +0100

    Add files via upload

---
+++ View/Poster_Sujet.php
@@ -0,0 +1,15 @@
+﻿<html>
+	<head>
+		<title>Forum - Creer un topic</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+
+	<?php 
+		session_start();
+		include("../Model/config.php");
+		include("../Model/users.php");
+
+?>
+</body>
+</html>
--- View/consulter_forum.php
+++ View/consulter_forum.php
@@ -6,13 +6,13 @@
 	<body>
 		<?php 
 		session_start();
-		include("../Model/config0php");
+		include("../Model/config.php");
 		include("../Model/users.php");
 		$titre=("Voir le Forum");
 		
 		$forum =(int) $_GET['f'];
 	// Récupération des champs de la bdd	
-$query=$bdd->prepare('Select Categorie, Section, Topic, Post, MessagePrive from Forum');
+$query=$bdd->prepare('Select Section, Categorie, Topic, Post, MessagePrive from Forum');
 	
 //Formatte les pages quand trop de messages		
 $TotalMessage= $data['forum_topic']+1;
@@ -38,7 +38,42 @@ for ($i = 1 ; $i <= $nombreDePages ; $i++)
 }
 
 echo '</p>';
-		?>
 
+// Bouton poster un nouveau sujet
+echo'<a href="./Poster_Sujet.php?action=nouveautopic&amp;f='.$forum.'">
+<button type="button" class="btn btn-primary">Nouveau sujet</button>';
+$query->CloseCursor();
+
+// Recupération des données sur le forum
+$query=$bdd->prepare('Select * from Topic ...');
+		
+?>
+
+<?php
+// Affichage des topics
+if($query->rowCount()>0){
+
+
+?>
+
+<table>
+<tr>
+<th><strong>Titre</strong></th>
+<th><strong>Auteur</strong></th>
+<th><strong>Heure</strong></th>
+</tr>
+
+<?php
+// On affiche chaque topic
+
+while($data=$query->fetch()){
+	// faire l'affichage avec les données
+}
+?>
+</table>
+<?php
+}
+$query->CloseCursor();
+?>
 	</body>
 </html>
