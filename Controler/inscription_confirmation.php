a10f12b0d64f9f549084ef45973cbba705bfd566
Yuoox <royal.lastchaos@gmail.com>
2017-01-03 09:26:40 +0100

    Add files via upload

---
+++ Controler/inscription_confirmation.php
@@ -0,0 +1,15 @@
+﻿<?php 
+
+	function inscription()
+	{
+		include_once('../Model/Users.php');
+		$users1 = new Users($_POST["pseudo"],$_POST["email"],$_POST["mdp"],$_POST["desc"],$_POST["ddn"],$_POST["type"]);
+		$sucessful = $users1->add_user($users1);
+		if($sucessful=='true')
+		include_once('../View/inscription_ok.php');
+		else
+			include_once('../View/inscription_error.php');
+	}
+	
+	inscription() ;
+?>
