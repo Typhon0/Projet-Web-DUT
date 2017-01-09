b466024132fe1bc4dd3eede538c702df4fc4c881
Typhon0 <sculier.loic@gmail.com>
2017-01-09 18:41:12 +0100

    search form

--- View/Poster_Sujet.php
+++ View/Poster_Sujet.php
@@ -1,15 +1,16 @@
-﻿<html>
-	<head>
-		<title>Forum - Creer un topic</title>
-		<link rel="stylesheet" type="text/css" href="style.css" />
-	</head>
-	<body>
+<html>
 
-	<?php 
+<head>
+    <title>Forum - Creer un topic</title>
+    <link rel="stylesheet" type="text/css" href="style.css" /> </head>
+
+<body>
+    <?php 
 		session_start();
 		include("../Model/config.php");
 		include("../Model/users.php");
 
 ?>
 </body>
+
 </html>
