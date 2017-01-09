6623870a48c0bf7e5c0eb8dc78a587bd880498d4
AsTrag0ld <anthony.defossez03@gmail.com>
2017-01-09 10:00:19 +0100

    Mise à jour et ajout envoi

--- View/Lire_message.php
+++ View/Lire_message.php
@@ -1,51 +1,49 @@
 <?php
 include('config.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link href="<?php echo $design; ?>/style.css" rel="stylesheet" title="Style" />
-        <title>Lecture d'un MP</title>
+        <link href=style.css" rel="stylesheet" title="Style" />
+        <title>Lecture d'un Message</title>
     </head>
     <body>
-        <div class="header">
-                <a href="<?php echo $url_home; ?>"><img src="<?php echo $design; ?>/images/logo.png" alt="Espace Membre" /></a>
-            </div>
+       
 <?php
 //On verifie si lutilisateur est connecte
-if(isset($_SESSION['username']))
+if(isset($_SESSION['login']))
 {
 //On verifie que lidentifiant de la discution est defini
-if(isset($_GET['id']))
+if(isset($_GET['idMessage']))
 {
-$id = intval($_GET['id']);
+$id = intval($_GET['idMessage']);
 //On recupere le titre et les narateurs de la discution
-$req1 = mysql_query('select title, user1, user2 from pm where id="'.$id.'" and id2="1"');
+$req1 = mysql_query('select idMessage, emeteteur, destinataire from Message where idMessage="'.$id);
 $dn1 = mysql_fetch_array($req1);
 //On verifie que la discution existe
 if(mysql_num_rows($req1)==1)
 {
 //On verifie que lutilisateur a le droit dafficher les messages
-if($dn1['user1']==$_SESSION['userid'] or $dn1['user2']==$_SESSION['userid'])
+if($dn1['emetteur']==$_SESSION['userid'] or $dn1['destinataire']==$_SESSION['userid'])
 {
 //La discution sera placee dans les messages lus
-if($dn1['user1']==$_SESSION['userid'])
+if($dn1['emetteur']==$_SESSION['userid'])
 {
-        mysql_query('update pm set user1read="yes" where id="'.$id.'" and id2="1"');
+        mysql_query('update Message set lu=true where idMessage="'.$id);
         $user_partic = 2;
 }
 else
 {
-        mysql_query('update pm set user2read="yes" where id="'.$id.'" and id2="1"');
+        mysql_query('update Message set lu=true where idMessage="'.$id);
         $user_partic = 1;
 }
 //On recupere la liste des messages
-$req2 = mysql_query('select pm.timestamp, pm.message, users.id as userid, users.username, users.avatar from pm, users where pm.id="'.$id.'" and users.id=pm.user1 order by pm.id2');
+$req2 = mysql_query('select Message.date_envoi, Message.contenu, Utilisateur.idUtilisateur as userid, Utilisateur.login from Message, Utilisateur where Message.idMessage="'.$id.'" and Utilisateur.idUtilisateur=Message.destinataire order by Message.date_envoi');
 //On verifie si lutilisateur a valide le formulaire de reponse
-if(isset($_POST['message']) and $_POST['message']!='')
+if(isset($_POST['contenu']) and $_POST['contenu']!='')
 {
-        $message = $_POST['message'];
+        $message = $_POST['contenu'];
         //On enleve lechappement si get_magic_quotes_gpc est active
         if(get_magic_quotes_gpc())
         {
@@ -54,18 +52,18 @@ if(isset($_POST['message']) and $_POST['message']!='')
         //On echape le message pour pouvoir le mettre dans une requette SQL
         $message = mysql_real_escape_string(nl2br(htmlentities($message, ENT_QUOTES, 'UTF-8')));
         //On envoi la reponse et le statut de la discution passe a non-lu pour lautre utilisateur
-        if(mysql_query('insert into pm (id, id2, title, user1, user2, message, timestamp, user1read, user2read)values("'.$id.'", "'.(intval(mysql_num_rows($req2))+1).'", "", "'.$_SESSION['userid'].'", "", "'.$message.'", "'.time().'", "", "")') and mysql_query('update pm set user'.$user_partic.'read="yes" where id="'.$id.'" and id2="1"'))
+        if(mysql_query('insert into Message (idMessage, destinataire, emetteur, contenu, lu, date_envoi)values("'.$id.'", "'.(intval(mysql_num_rows($req2))+1).'", "", "'.$_SESSION['userid'].'", "", "'.$message.'", "'.time().'", "", "")') and mysql_query('update Message set Utilisateur'.$user_partic.'lu=true where idMessage="'.$id))
         {
 ?>
-<div class="message">Votre message a bien &eacute;t&eacute; envoy&eacute;.<br />
-<a href="read_pm.php?id=<?php echo $id; ?>">Retour &agrave; la discussion</a></div>
+<div class="message">Votre message a bien ete envoye.<br />
+<a href="Lire_message.php?id=<?php echo $id; ?>">Retour a la discussion</a></div>
 <?php
         }
         else
         {
 ?>
 <div class="message">Une erreur c'est produite lors de l'envoi du message.<br />
-<a href="read_pm.php?id=<?php echo $id; ?>">Retour &agrave; la discussion</a></div>
+<a href="Lire_message.php?id=<?php echo $id; ?>">Retour a la discussion</a></div>
 <?php
         }
 }
@@ -74,7 +72,7 @@ else
 //On affiche la liste des messages
 ?>
 <div class="content">
-<h1><?php echo $dn1['title']; ?></h1>
+<?php echo $dn1['objet']; ?>
 <table class="messages_table">
         <tr>
         <th class="author">Utilisateur</th>
@@ -85,23 +83,19 @@ while($dn2 = mysql_fetch_array($req2))
 {
 ?>
         <tr>
-        <td class="author center"><?php
-if($dn2['avatar']!='')
-{
-        echo '<img src="'.htmlentities($dn2['avatar']).'" alt="Image Perso" style="max-width:100px;max-height:100px;" />';
-}
-?><br /><a href="profile.php?id=<?php echo $dn2['userid']; ?>"><?php echo $dn2['username']; ?></a></td>
-        <td class="left"><div class="date">Date d'envoi: <?php echo date('d/m/Y H:i:s' ,$dn2['timestamp']); ?></div>
-        <?php echo $dn2['message']; ?></td>
+        <td class="author center">
+		><br /><a href="profile.php?id=<?php echo $dn2['userid']; ?>"><?php echo $dn2['login']; ?></a></td>
+        <td class="left"><div class="date">Date d'envoi: <?php echo date('d/m/Y H:i:s' ,$dn2['date_envoi']); ?></div>
+        <?php echo $dn2['contenu']; ?></td>
     </tr>
 <?php
 }
 //On affiche le formulaire de reponse
 ?>
 </table><br />
-<h2>R&eacute;pondre</h2>
+<h2>Repondre</h2>
 <div class="center">
-    <form action="read_pm.php?id=<?php echo $id; ?>" method="post">
+    <form action="Lire_message.php?id=<?php echo $id; ?>" method="post">
         <label for="message" class="center">Message</label><br />
         <textarea cols="40" rows="5" name="message" id="message"></textarea><br />
         <input type="submit" value="Envoyer" />
@@ -113,7 +107,7 @@ if($dn2['avatar']!='')
 }
 else
 {
-        echo '<div class="message">Vous n\'avez pas le droit d\'acc&eacute;der &agrave; cette page.</div>';
+        echo '<div class="message">Vous n\'avez pas le droit d\'acceder a cette page.</div>';
 }
 }
 else
@@ -123,14 +117,13 @@ else
 }
 else
 {
-        echo '<div class="message">L\'identifiant du message n\'est pas d&eacute;fini.</div>';
+        echo '<div class="message">L\'identifiant du message n\'est pas defini.</div>';
 }
 }
 else
 {
-        echo '<div class="message">Vous devez &ecirc;tre connect&eacute; pour acc&eacute;der &agrave; cette page.</div>';
+        echo '<div class="message">Vous devez etre connecte pour acceder a cette page.</div>';
 }
 ?>
-                <div class="foot"><a href="list_pm.php">Retour &agrave; mes messages priv&eacute;s</a> - <a href="http://www.supportduweb.com/">Support du Web</a></div>
-        </body>
+                 </body>
 </html>
--- View/messagerie.php
+++ View/messagerie.php
@@ -30,137 +30,139 @@ $req1 = mysql_query('select m1.idMessage, m1.destinataire, m1.emetteur, count(m2
 
 $req2 = mysql_query('select m1.idMessage, m1.destinataire, m1.emetteur, count(m2.idMessage) as reps,m1.contenu,m1.objet,m1.lu,m1.date_envoi, Utilisateur.idUtilisateur as userid, Utilisateur.login from Message as m1, Message as m2,Utilisateur where ((m1.emetteur="'.$_SESSION['userid'].'" and m1.lu=true and Utilisateur.idUtilisateur=m1.destinataire) or (m1.destinataire="'.$_SESSION['userid'].'" and m1.lu=true and Utilisateur.idUtilisateur=m1.emetteur)) and m2.idMessage=m1.idMessage group by m1.idMessage order by m1.idMessage desc');
                 
-               ?>
-                        <div class="row">
-                            <div class="col-sm-3 col-md-2"> </div>
-                            <div class="col-sm-9 col-md-10">
-                                <div class="pull-right">
-                                    <button type="button" class="btn btn-default" data-toggle="tooltip" title="Refresh"> &nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-refresh"></span>&nbsp;&nbsp;&nbsp;</button>
-                                </div>
+               ?> 
+                
+                    <div class="row">
+                        <div class="col-sm-3 col-md-2"> </div>
+                        <div class="col-sm-9 col-md-10">
+                            <div class="pull-right">
+                                <button type="button" class="btn btn-default" data-toggle="tooltip" title="Refresh"> &nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-refresh"></span>&nbsp;&nbsp;&nbsp;</button>
                             </div>
                         </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3 col-md-2"> <a href="#" class="btn btn-danger btn-sm btn-block" role="button"><i class="glyphicon glyphicon-edit"></i> Nouveau</a>
-                                <hr>
-                                <ul class="nav nav-pills nav-stacked">
-                                    <li class="active"><a href="#"><span class="badge pull-right">32</span> Boîte de réception </a> </li>
-                                    <li><a href="#">Envoyé</a></li>
-                                    <li><a href="#">Corbeille</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-sm-9 col-md-10">
-                                <!-- Tab panes -->
-                                <div class="tab-content">
-                                    <div class="tab-pane fade in active" id="home">
-                                        <div class="list-group">
-                                            <a href="#" class="list-group-item"> <span class="glyphicon glyphicon-star-empty"></span><span class="name" style="min-width: 120px;
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3 col-md-2"> <a href="#" class="btn btn-danger btn-sm btn-block" role="button"><i class="glyphicon glyphicon-edit"></i> Nouveau</a>
+                            <hr>
+                            <ul class="nav nav-pills nav-stacked">
+                                <li class="active"><a href="#"><span class="badge pull-right">32</span> Boîte de réception </a> </li>
+                                <li><a href="#">Envoyé</a></li>
+                                <li><a href="#">Corbeille</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-9 col-md-10">
+                            <!-- Tab panes -->
+                            <div class="tab-content">
+                                <div class="tab-pane fade in active" id="home">
+                                    <div class="list-group">
+                                        <a href="#" class="list-group-item"> <span class="glyphicon glyphicon-star-empty"></span><span class="name" style="min-width: 120px;
                                 display: inline-block;">Mark Otto</span> <span class="">Nice work on the lastest version</span> <span class="text-muted" style="font-size: 11px;">- More content here</span> <span class="badge">12:10 AM</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip">
                                 </span></span>
-                                            </a>
-                                        </div>
+                                        </a>
                                     </div>
-                                    <div class="tab-pane fade in" id="profile">
-                                        <div class="list-group">
-                                            <div class="list-group-item"> <span class="text-center">This tab is empty.</span> </div>
-                                        </div>
-                                    </div>
-                                    <div class="tab-pane fade in" id="messages"> ...</div>
-                                    <div class="tab-pane fade in" id="settings"> This tab is empty.</div>
                                 </div>
-                                <div class="row-md-12"> </div>
+                                <div class="tab-pane fade in" id="profile">
+                                    <div class="list-group">
+                                        <div class="list-group-item"> <span class="text-center">This tab is empty.</span> </div>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade in" id="messages"> ...</div>
+                                <div class="tab-pane fade in" id="settings"> This tab is empty.</div>
                             </div>
+                            <div class="row-md-12"> </div>
                         </div>
+                    </div>
             </div>
-            <?php
+
+<?php
 //On affiche la liste des messages non-lus
 while($dn1 = mysql_fetch_array($req1))
 {
 ?>
-                <tr>
-                    <td>
-                        <a href="Lire_Message.php?id=<?php echo $dn1['idMessage']; ?>">
-                            <?php echo htmlentities($dn1['Objet'], ENT_QUOTES, 'UTF-8'); ?>
-                        </a>
-                    </td>
-                    <td>
-                        <?php echo $dn1['reps']-1; ?>
-                    </td>
-                    <td>
-                        <a href="consulter_profil.php?id=<?php echo $dn1['userid']; ?>">
-                            <?php echo htmlentities($dn1['login'], ENT_QUOTES, 'UTF-8'); ?>
-                        </a>
-                    </td>
-                    <td>
-                        <?php echo date('d/m/Y H:i:s' ,$dn1['date_envoi']); ?>
-                    </td>
-                </tr>
-                <?php
+                        <tr>
+                            <td>
+                                <a href="Lire_Message.php?id=<?php echo $dn1['idMessage']; ?>">
+                                    <?php echo htmlentities($dn1['Objet'], ENT_QUOTES, 'UTF-8'); ?>
+                                </a>
+                            </td>
+                            <td>
+                                <?php echo $dn1['reps']-1; ?>
+                            </td>
+                            <td>
+                                <a href="consulter_profil.php?id=<?php echo $dn1['userid']; ?>">
+                                    <?php echo htmlentities($dn1['login'], ENT_QUOTES, 'UTF-8'); ?>
+                                </a>
+                            </td>
+                            <td>
+                                <?php echo date('d/m/Y H:i:s' ,$dn1['date_envoi']); ?>
+                            </td>
+                        </tr>
+                        <?php
 }
 //S'il aucun message non-lu, on le dit
 if(intval(mysql_num_rows($req1))==0)
 {
 ?>
-                    <tr>
-                        <td colspan="4">Vous n'avez aucun message non-lu.</td>
-                    </tr>
-                    <?php
-}
-?>
-                        </table>
-                        <br />
-                        <h3>Messages lus(<?php echo intval(mysql_num_rows($req2)); ?>):</h3>
-                        <table>
                             <tr>
-                                <th class="title_cell">Titre</th>
-                                <th>Nb. Réponses</th>
-                                <th>Participant</th>
-                                <th>Date d'envoi</th>
+                                <td colspan="4">Vous n'avez aucun message non-lu.</td>
                             </tr>
                             <?php
+}
+?>
+                                </table>
+                                <br />
+                                <h3>Messages lus(<?php echo intval(mysql_num_rows($req2)); ?>):</h3>
+                                <table>
+                                    <tr>
+                                        <th class="title_cell">Titre</th>
+                                        <th>Nb. Réponses</th>
+                                        <th>Participant</th>
+                                        <th>Date d'envoi</th>
+                                    </tr>
+                                    <?php
 //On affiche la liste des messages lus
 while($dn2 = mysql_fetch_array($req2))
 {
 ?>
-                                <tr>
-                                    <td>
-                                        <a href="Lire_message.php?id=<?php echo $dn2['idMessage']; ?>">
-                                            <?php echo htmlentities($dn2['Objet'], ENT_QUOTES, 'UTF-8'); ?>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <?php echo $dn2['reps']-1; ?>
-                                    </td>
-                                    <td>
-                                        <a href="consulter_profil.php?id=<?php echo $dn2['userid']; ?>">
-                                            <?php echo htmlentities($dn2['login'], ENT_QUOTES, 'UTF-8'); ?>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <?php echo date('d/m/Y H:i:s' ,$dn2['date_envoi']); ?>
-                                    </td>
-                                </tr>
-                                <?php
+                                        <tr>
+                                            <td>
+                                                <a href="Lire_message.php?id=<?php echo $dn2['idMessage']; ?>">
+                                                    <?php echo htmlentities($dn2['Objet'], ENT_QUOTES, 'UTF-8'); ?>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <?php echo $dn2['reps']-1; ?>
+                                            </td>
+                                            <td>
+                                                <a href="consulter_profil.php?id=<?php echo $dn2['userid']; ?>">
+                                                    <?php echo htmlentities($dn2['login'], ENT_QUOTES, 'UTF-8'); ?>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <?php echo date('d/m/Y H:i:s' ,$dn2['date_envoi']); ?>
+                                            </td>
+                                        </tr>
+                                        <?php
 }
 //Sil na aucun message lu, on le dit
 if(intval(mysql_num_rows($req2))==0)
 {
 ?>
-                                    <tr>
-                                        <td colspan="4">Vous n'avez aucun message lu.</td>
-                                    </tr>
-                                    <?php
+                                            <tr>
+                                                <td colspan="4">Vous n'avez aucun message lu.</td>
+                                            </tr>
+                                            <?php
 }
 ?>
-                        </table>
-                        <?php
+                                </table>
+                                <?php
 }
 else
 {
         echo 'Non connecter';
 }
 ?>
-                            <div>
+            <div>
     </body>
     <!-- jQuery -->
     <script src="../js/jquery.js"></script>
