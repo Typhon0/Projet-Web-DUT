bf13930a411080a5d211c6ca2ebc7f161752c8ec
Yuoox <royal.lastchaos@gmail.com>
2017-01-09 09:48:25 +0100

    mise à jour

--- Controler/Connexion.php
+++ Controler/Connexion.php
@@ -1,13 +1,15 @@
 <?php
 function connection(){
-	$login =$_POST['login'];
-	$pass = $_POST['mdp'];
+	
+	$login = $_POST["email"];
+	$pass = $_POST["mdp"];
 	include_once('../Model/Users.php');
 	
-	$usr = get_user($login);
-	if ($usr->mdp == $pass) {
-		$_SESSION['user_id'] = $usr->id;
-		$_SESSION['username'] = $login;
+	$usr = Users::get_info_connect_user($login);
+	if ($usr['mdp'] == $pass) {
+		echo "connecté" ;
+		$_SESSION['user_id'] = $usr['idUtilisateur'];
+		$_SESSION['username'] = $usr['login'];
 		$_SESSION['logged'] = true;
 		return true;
 	} else {
@@ -15,28 +17,8 @@ function connection(){
 		$_SESSION['logged'] = false;
 		return false;
 	}
-	
- }
+	}
  
- /*function login_check($mysqli) {
-	 include_once('../Model/config.php');
-    // Check if all session variables are set 
-    if (isset($_SESSION['user_id'], $_SESSION['username'])) {
-        $user_id = $_SESSION['user_id'];
-        $username = $_SESSION['username'];
-
-        // Get the user-agent string of the user.
-        $user_browser = $_SERVER['HTTP_USER_AGENT'];
-
-        if ($stmt = $bdd->prepare("SELECT mdp 
-				      FROM Utilisateur 
-				      WHERE idUtilisateur = ? LIMIT 1")) {
-            // Bind "$user_id" to parameter. 
-            $stmt->bindValue(1, $user_id, PDO::PARAM_STR);
-            $stmt->execute();   // Execute the prepared query.
-            $stmt->store_result();
-
-            
-}
-*/
+ 	connection();
+ 
 ?>
