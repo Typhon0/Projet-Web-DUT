ff000600888e3856bbe44a7435de78b7b619859a
Typhon0 <sculier.loic@gmail.com>
2017-01-09 19:28:00 +0100

    fix css path

--- Model/filtres.php
+++ Model/filtres.php
@@ -1,8 +1,8 @@
 <?php
 	function filtreService($services){
 		$service = $services;
-		include('../Model/config.php');
-		$req = $bdd->prepare('Select * from annonce where demandeur IN (Select idUtilisateur from utilisateurservice where idService IN (Select idService from service where nom LIKE ?))');
+		include('config.php');
+		$req = $bdd->prepare('Select * from Annonce where demandeur IN (Select idUtilisateur from UtilisateurService where idService IN (Select idService from service where nom LIKE ?))');
 		$req->execute(array($service));
 		$resultat = $req->fetchAll();
 		return $resultat;
@@ -22,7 +22,7 @@
 		$prixmin = $Min;
 		$prixmax = $Max;
 		include('../Model/config.php');
-		$req = $bdd->prepare('SELECT * FROM annonce where prix > ? and prix < ?');
+		$req = $bdd->prepare('SELECT * FROM Annonce where prix > ? and prix < ?');
 		$req->execute(array($prixmin,$prixmax));
 		$result = $req->fetchAll();
 		return $result;
@@ -41,7 +41,7 @@
 	function filtreLieu($newLieu){
 		$lieu = $newLieu;
 		include("../Model/config.php");
-		$req = $bdd->prepare('SELECT * FROM annonce where lieu = ?');
+		$req = $bdd->prepare('SELECT * FROM Annonce where lieu = ?');
 		$req->execute(array($lieu));
 		$resultat = $req->fetchAll();
 		return $resultat;
--- View/resultats_recherche.php
+++ View/resultats_recherche.php
@@ -11,11 +11,11 @@
         <meta name="author" content="">
         <title>Logo Nav - Start Bootstrap Template</title>
         <!-- Bootstrap Core CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
         <!-- Custom CSS -->
-        <link href="css/style.css" rel="stylesheet">
-        <link href="css/hover-min.css" rel="stylesheet">
-        <link href="css/swiper.min.css" rel="stylesheet">
+        <link href="../css/style.css" rel="stylesheet">
+        <link href="../css/hover-min.css" rel="stylesheet">
+        <link href="../css/swiper.min.css" rel="stylesheet">
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
@@ -27,10 +27,8 @@
     <body>
         <?php require("../View/menu.php"); ?>
             <!-- Page Content -->
-            <div class="container">
-                
-            </div>
-				<?php
+            <div class="container"> </div>
+            <?php
 				include('../Model/filtres.php');
 				include('../Model/fonctions_annonces.php');
 				$filtreService = $_POST['service'];
@@ -43,62 +41,62 @@
 				} else if (!empty($filtreLieu)) {
 					$lesAnnonces = recherche_annonce_lieu($filtreLieu);
 				} else { ?>
-					<h3>Veuillez remplir au moins un champ</h3>
-					<?php 
-					header('Location: ../View/all_annonces.php');
+                <h3>Veuillez remplir au moins un champ</h3>
+                <?php 
+					header('Location: all_annonces.php');
 				}
 				foreach ($lesAnnonces as $annonce) {
 				?>
-                <div id="listannonceP" class="row">
-                    <div class="col-xs-12">
-                        <div class="col-xs-4"></div>
-                        <div class="col-xs-4">
-                            <div id="listannonce" class="row">
-                                <a href="#">
-                                    <div class="col-xs-3 col-sm-2 text-center no-padding">
-                                        <div class="search-icon"></div>
-                                    </div>
-                                    <div class="col-xs-9 col-sm-10">
-                                        <h4 class="title">
+                    <div id="listannonceP" class="row">
+                        <div class="col-xs-12">
+                            <div class="col-xs-4"></div>
+                            <div class="col-xs-4">
+                                <div id="listannonce" class="row">
+                                    <a href="#">
+                                        <div class="col-xs-3 col-sm-2 text-center no-padding">
+                                            <div class="search-icon"></div>
+                                        </div>
+                                        <div class="col-xs-9 col-sm-10">
+                                            <h4 class="title">
                                             <?php echo $annonce['titre'] ?>                                       </h4>
-                                        <div class="username"> Type de service : <span class="capitalize firstname">
+                                            <div class="username"> Type de service : <span class="capitalize firstname">
 											<?php $service = get_nomService($annonce['service']);
 													echo $service ?></span></div>
-                                        <div class="username"> Utilisateur : <span class="capitalize firstname">
+                                            <div class="username"> Utilisateur : <span class="capitalize firstname">
 											<?php $username = get_username_demandeur($annonce['demandeur']);
 													echo $username ?></span></div>
-                                        <div class="budget"> Budget : <b>
+                                            <div class="budget"> Budget : <b>
                                             <?php echo $annonce['prix'] ?> €</b> </div>
-                                        <div class="duration"> Lieu : <b>
+                                            <div class="duration"> Lieu : <b>
 											<?php echo $annonce['lieu'] ?> </b> </div>
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <?php } ?>
-                    </div>
-                    <!-- /.container -->
-                    <!-- jQuery -->
-                    <script src="js/jquery.js"></script>
-                    <!-- Bootstrap Core JavaScript -->
-                    <script src="js/bootstrap.min.js"></script>
-                    <!-- Swipper -->
-                    <script src="js/swiper.min.js"></script>
-                    <!-- Initialize Swiper -->
-                    <script>
-                        var swiper = new Swiper('.swiper-container', {
-                            pagination: '.swiper-pagination'
-                            , paginationClickable: true
-                            , autoplay: 3000
-                            , loop: true
-                            , nextButton: '.swiper-button-next'
-                            , prevButton: '.swiper-button-prev'
-                            , spaceBetween: 30
-                            , effect: 'fade'
-                        });
-                    </script>
+                    <?php } ?>
+                        </div>
+                        <!-- /.container -->
+                        <!-- jQuery -->
+                        <script src="../js/jquery.js"></script>
+                        <!-- Bootstrap Core JavaScript -->
+                        <script src="../js/bootstrap.min.js"></script>
+                        <!-- Swipper -->
+                        <script src="../js/swiper.min.js"></script>
+                        <!-- Initialize Swiper -->
+                        <script>
+                            var swiper = new Swiper('.swiper-container', {
+                                pagination: '.swiper-pagination'
+                                , paginationClickable: true
+                                , autoplay: 3000
+                                , loop: true
+                                , nextButton: '.swiper-button-next'
+                                , prevButton: '.swiper-button-prev'
+                                , spaceBetween: 30
+                                , effect: 'fade'
+                            });
+                        </script>
     </body>
 
     </html>
